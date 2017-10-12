43d4806d993eb712f49a32e54646d8c058a569ea
Alex MOINIER <amoinier@student.42.fr>
2017-10-12 10:16:45 +0200

    D07/ex01 const _ init D08

--- D07/ex01/iter.cpp
+++ D07/ex01/iter.cpp
@@ -6,14 +6,14 @@
 /*   By: amoinier <amoinier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/11 13:52:45 by amoinier          #+#    #+#             */
-/*   Updated: 2017/10/11 16:45:43 by amoinier         ###   ########.fr       */
+/*   Updated: 2017/10/12 10:13:19 by amoinier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 template<typename T>
-void iter(T * array, int length, void (*f)(T))
+void iter(T * array, int length, void (*f)(T const &))
 {
 	for (int i = 0; i < length; i++) {
 		f(array[i]);
@@ -31,8 +31,8 @@ int main(void) {
 	std::string names[4] = {"Jimi", "Alex", "Florian", "Alexandre"};
 	int nbr[4] = {1, 2, 3, 4};
 
-	::iter<int>(nbr, 4, print);
-	::iter<std::string>(names, 4, print);
+	::iter(nbr, 4, print);
+	::iter(names, 4, print);
 
 	return (0);
 }
